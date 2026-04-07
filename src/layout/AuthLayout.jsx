21e2d244fafe5ac0f81d32d7b1eedd8ce8a7d5a8
jijo-george <abhilash@technowavegroup.com>
2026-04-07 13:13:01 +0530

    update

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,8 +1,17 @@
-const AuthLayout = () => {
+import LoginForm from "../features/auth/components/LoginForm";
+
+const AuthLayout = ({ children }) => {
   return (
-    <div className="flex flex-row">
-      dsddsds
-      <div></div>
+    <div className ="grid grid-cols-2 min-h-screen min-w-full">
+      {/* Logo Section - Left Side */}
+      <div className = "">
+        
+      </div>
+
+      {/* Login Section - Right Side */}
+      <div className="">
+        <div>Welcome Back</div>
+      </div>
     </div>
   );
 };
