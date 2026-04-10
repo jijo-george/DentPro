c8e4619b78009de76c4920ce20709bb0f26f9980
jijo-george <abhilash@technowavegroup.com>
2026-04-10 17:46:07 +0530

    layout

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,18 +1,32 @@
 import LoginForm from "../features/auth/components/LoginForm";
+import "../App.css";
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className ="grid grid-cols-2 min-h-screen min-w-full">
+    <div className="auth-layout">
       {/* Logo Section - Left Side */}
-      <div className = "">
-        
+      <div className="logo-section">
+        <div className="bg-circle bg1" />
+        <div className="bg-circle bg2" />
+
+        <div className="Logo">
+          <div className="Logo-icon">🦷</div>
+          <div className="Logo-text flex flex-col items-start">
+            <h1 className="Logo-text-main">DentPro</h1>
+            <p>Your Dental Care Partner</p>
+          </div>
+        </div>
+
+        <div className="bg-circle bg3" />
+        <div className="bg-circle bg4" />
       </div>
 
       {/* Login Section - Right Side */}
-      <div className="">
+      <div className="flex items-center justify-center">
         <div>Welcome Back</div>
       </div>
     </div>
   );
 };
+
 export default AuthLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    "bg-[#0f4d41]",
+    "bg-[#2a806b]/20",
+    "-left-40",
+    "-top-40",
+    "h-[900px]",
+    "w-[900px]",
   ],
   theme: {
     extend: {},
