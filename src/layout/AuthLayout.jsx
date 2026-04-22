dbe23ffa0d7d94b79d96b008d9ea1cb017ff0d7a
jijo-george <abhilash@technowavegroup.com>
2026-04-22 22:47:35 +0530

    layout update

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -11,8 +11,12 @@ const AuthLayout = ({ children }) => {
 
         <div className="Logo">
           <div className="Logo-icon">🦷</div>
-          <div className="Logo-text flex flex-col items-start">
+          {/* <div className="Logo-text flex flex-col items-start">
             <h1 className="Logo-text-main">DentPro</h1>
+            <p>Your Dental Care Partner</p>
+          </div> */}
+          <div className="Lg-text">
+            <div className="Lg-text-main">Dent Pro</div>
             <p>Your Dental Care Partner</p>
           </div>
         </div>
@@ -22,8 +26,17 @@ const AuthLayout = ({ children }) => {
       </div>
 
       {/* Login Section - Right Side */}
-      <div className="flex items-center justify-center">
-        <div>Welcome Back</div>
+      <div className="auth-right">
+        <div className="form-card">
+          <div className="form-header">
+            <div className="welcome-row">
+              <h2>Welcome back</h2>
+              <span className="wave">👋</span>
+            </div>
+            <p>Sign in to your DentalCare Pro account</p>
+          </div>
+          <div className="role-label">Signing in as</div>
+        </div>
       </div>
     </div>
   );
