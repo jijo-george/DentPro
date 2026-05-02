ad781bb906d6a14069e905dcfe3a5bf27c457967
jijo-george <abhilash@technowavegroup.com>
2026-05-03 01:53:33 +0530

    login state

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,7 +1,14 @@
 import LoginForm from "../features/auth/components/LoginForm";
 import "../App.css";
+import { useState } from "react";
+const roles = [
+  { id: "admin", name: "System Admin", icon: "👤" },
+  { id: "Cl-Admin", name: "Clinic Admin", icon: "👨‍⚕️" },
+  { id: "Cl-user", name: "Clinic User", icon: "👩‍💼" },
+];
 
 const AuthLayout = ({ children }) => {
+  const [activeRole, setActiveRole] = useState("admin");
   return (
     <div className="auth-layout">
       {/* Logo Section - Left Side */}
@@ -11,10 +18,7 @@ const AuthLayout = ({ children }) => {
 
         <div className="Logo">
           <div className="Logo-icon">🦷</div>
-          {/* <div className="Logo-text flex flex-col items-start">
-            <h1 className="Logo-text-main">DentPro</h1>
-            <p>Your Dental Care Partner</p>
-          </div> */}
+
           <div className="Lg-text">
             <div className="Lg-text-main">Dent Pro</div>
             <p>Your Dental Care Partner</p>
@@ -52,18 +56,18 @@ const AuthLayout = ({ children }) => {
           </div>
         </div>
         <div className="auth-footer">
-            <div className="stat">
-              <div className="stat-num">1000+</div>
-              <div className="stat-label">Happy Patients</div>
-            </div>
-            <div className="stat">
-              <div className="stat-num">50+</div>
-              <div className="stat-label">Dental Clinics</div>
-            </div>
-            <div className="stat">
-              <div className="stat-num">100+</div>
-              <div className="stat-label">Happy Dentists</div>
-            </div>
+          <div className="stat">
+            <div className="stat-num">1000+</div>
+            <div className="stat-label">Happy Patients</div>
+          </div>
+          <div className="stat">
+            <div className="stat-num">50+</div>
+            <div className="stat-label">Dental Clinics</div>
+          </div>
+          <div className="stat">
+            <div className="stat-num">100+</div>
+            <div className="stat-label">Happy Dentists</div>
+          </div>
         </div>
         <div className="bg-circle bg3" />
         <div className="bg-circle bg4" />
@@ -80,6 +84,20 @@ const AuthLayout = ({ children }) => {
             <p>Sign in to your DentalCare Pro account</p>
           </div>
           <div className="role-label">Signing in as</div>
+          <div className="role-tabs">
+            {roles.map((role) => (
+              <button
+                key={role.id}
+                className={
+                  "role-tab" + (activeRole === role.id ? " active" : "")
+                }
+                onClick={() => setActiveRole(role.id)}
+              >
+                <span className="icon">{role.icon}</span>
+                {role.name}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
